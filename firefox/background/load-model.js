d39cf4488adb2561e1138420f58b983af434f66b
ngbusca <ngbusca@in2p3.fr>
2020-12-06 06:34:33 +0200

    firefox version

---
+++ firefox/background/load-model.js
@@ -0,0 +1,9 @@
+var model = undefined;
+browser.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    sendResponse(model.detect(request.img, 20, 0.3));
+});
+
+cocoSsd.load().then((res) => {
+    model = res;
+    console.log("Model loaded");
+});
