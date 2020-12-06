c0037f38b6fb4c91bb57ad36102260773ffad03e
ngbusca <ngbusca@in2p3.fr>
2020-12-06 10:24:50 +0200

    cleanup

--- background/load-model.js
+++
@@ -1,9 +0,0 @@
-var model = undefined;
-browser.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    sendResponse(model.detect(request.img, 20, 0.3));
-});
-
-cocoSsd.load().then((res) => {
-    model = res;
-    console.log("Model loaded");
-});
